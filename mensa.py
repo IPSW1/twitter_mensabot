894f195fe75fd8dcc69f7f643693d73d3b9b6816
Marco Reichenberger <IPSW1@users.noreply.github.com>
2017-03-10 18:25:33 +0100

    Add date method

--- mensa.py
+++ mensa.py
@@ -1,11 +1,17 @@
 import requests
 import csv
+import time
 
+
+def get_date():
+    return time.strftime("%d.%m.%Y")
 
 url = "https://www.stwno.de/infomax/daten-extern/csv/UNI-P/10.csv"
 response = requests.get(url)
 menu = response.content.decode('utf-8', 'ignore') # FIXME: don't ignore umlauts
 
+date = get_date()
 menureader = csv.reader(menu.splitlines(), delimiter=';')
 for row in menureader:
-    print(row)
+    if(row[0] == date):
+        print(row)
