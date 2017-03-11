e5fb8aa91e60755f9c9400d72e24c7243f1e9c7e
Marco Reichenberger <IPSW1@users.noreply.github.com>
2017-03-11 18:18:46 +0100

    Add twitter

--- mensa.py
+++ mensa.py
@@ -2,52 +2,95 @@ import requests
 import csv
 import datetime
 import re
+import tweepy
+
+# twitter api credentials
+consumer_key = ""
+consumer_secret = ""
+access_token = ""
+access_token_secret = ""
+
+# twitter api setup
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+twit_api = tweepy.API(auth)
+
+
+def main():
+    # get current date and week number
+    now = datetime.datetime.now()
+    date = get_date(now)
+    week = get_week(now)
+
+    # menu is saved as "[week number].csv"
+    url = "https://www.stwno.de/infomax/daten-extern/csv/UNI-P/{}.csv".format(week)
+    response = requests.get(url)
+    menu = response.content.decode('utf-8', 'ignore') # FIXME: don't ignore umlauts
+
+    # format menu and only use data for current day
+    menureader = csv.reader(menu.splitlines(), delimiter=';')
+    meals = []
+    for row in menureader:
+       if(row[0] == date):
+            meals.append([item for item in row])
+
+    # only post meals if canteen is open
+    if meals != []:
+        post = "Speiseplan für heute, den {}\n".format(date)
+        post += "(https://stwno.de/de/gastronomie/speiseplan/uni-passau):"
+        twit_api.update_status(status=post)
+
+        # soups
+        tweet_menu(meals, "S")
+        # main dish
+        tweet_menu(meals, "HG")
+        # side dishes
+        tweet_menu(meals, "B")
+        # desserts
+        tweet_menu(meals, "N")
+
+        post = "-------------------------------\nGuten Appetit!"
+        twit_api.update_status(status=post)
+
+    else:
+        post = "Heute kein Mensabetrieb"
+        twit_api.update_status(status=post)
+
 
 def get_date(dt):
     return dt.strftime("%d.%m.%Y")
 
+
 def get_week(dt):
     return datetime.date(dt.year, dt.month, dt.day).isocalendar()[1]
 
-def print_menu(menu, type):
+
+def tweet_menu(menu, meal_type):
+    if meal_type == "S":
+        tweet_menu_type(menu, meal_type)
+    elif meal_type == "HG":
+        tweet_menu_type(menu, meal_type)
+    elif meal_type == "B":
+        tweet_menu_type(menu, meal_type)
+    elif meal_type == "N":
+        tweet_menu_type(menu, meal_type)
+
+
+def tweet_menu_type(menu, meal_type):
+    index = 1
     for item in menu:
-        if item[2].startswith(type):
-            print(re.sub(r'\([^)]*\)', '',item[3]))
+        if item[2].startswith(meal_type):
+            post = item[2] +":\n"
 
-# get current date and week number
-now = datetime.datetime.now()
-date = get_date(now)
-week = get_week(now)
-
-date = "09.03.2017" # temporary fixed date for testing
-
-# menu is saved as "[week number].csv"
-url = "https://www.stwno.de/infomax/daten-extern/csv/UNI-P/{}.csv".format(week)
-response = requests.get(url)
-menu = response.content.decode('utf-8', 'ignore') # FIXME: don't ignore umlauts
+            post += re.sub(r'\([^)]*\)', '',item[3])    #filter out extra information in brackets (like additionals)
+            post += "\n"
+            if item[6] != "0,00":
+                post += "{}€".format(item[6])
+            else:
+                post += "Kein Preis angegeben"
+            twit_api.update_status(status=post)
+            index += 1
 
 
-menureader = csv.reader(menu.splitlines(), delimiter=';')
-meals = []
-for row in menureader:
-   if(row[0] == date):
-        meals.append([item for item in row])
-
-# soups
-print("Suppe:")
-print_menu(meals, "S")
-print()
-
-# main dish
-print("Hauptgerichte:")
-print_menu(meals, "HG")
-print()
-
-# side dishes
-print("Beilagen:")
-print_menu(meals, "B")
-print()
-
-# desserts
-print("Nachspeisen:")
-print_menu(meals, "N")
+if __name__ == '__main__':
+    main()
