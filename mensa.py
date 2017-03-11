3ce720d60e8b77e78b8d3d6ae6b2d856e7b62b12
Marco Reichenberger <IPSW1@users.noreply.github.com>
2017-03-11 12:27:27 +0100

    Change time to datetime

--- mensa.py
+++ mensa.py
@@ -1,17 +1,27 @@
 import requests
 import csv
-import time
+import datetime
 
 
-def get_date():
-    return time.strftime("%d.%m.%Y")
+def get_date(dt):
+    return "{}.{:02}.{}".format(dt.day, dt.month, dt.year)
 
-url = "https://www.stwno.de/infomax/daten-extern/csv/UNI-P/10.csv"
+def get_week(dt):
+    return datetime.date(dt.year, dt.month, dt.day).isocalendar()[1]
+
+# get current date and week number
+now = datetime.datetime.now()
+date = get_date(now)
+week = get_week(now)
+
+# menu is saved as "[week number].csv"
+url = "https://www.stwno.de/infomax/daten-extern/csv/UNI-P/{}.csv".format(week)
 response = requests.get(url)
 menu = response.content.decode('utf-8', 'ignore') # FIXME: don't ignore umlauts
 
-date = get_date()
+
 menureader = csv.reader(menu.splitlines(), delimiter=';')
+meals = []
 for row in menureader:
-    if(row[0] == date):
+   if(row[0] == date):
         print(row)
